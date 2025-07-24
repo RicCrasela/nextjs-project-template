86c0f1f254e24d64cfdce7f5bc62cb743f8d92ef
RicCrasela <167690237+RicCrasela@users.noreply.github.com>
2025-07-24 20:13:24 +0700

    Add .next/static/chunks/src_app_profile_page_tsx_c9c271c3._.js 1753362802934

---
+++ .next/static/chunks/src_app_profile_page_tsx_c9c271c3._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_861cf69d._.js"
+],
+    source: "dynamic"
+});
